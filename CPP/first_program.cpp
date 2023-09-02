434bb4f8f6bda119714509b88a6772633d54ea99
John Lawniczak <lawniczak.john@gmail.com>
2023-09-02 16:30:54 -0400

    add first program

---
+++ CPP/first_program.cpp
@@ -0,0 +1,12 @@
+/* Woo my first C== text program */
+
+#include <iostream>
+using namespace std;
+
+// Another comment sample
+int main()
+{
+    cout << "C++ is my jam babycakes\n";
+
+    return 0;
+}
